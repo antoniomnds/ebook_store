efc14756605d7ed448db44e18c9fa339be1148f9
Antonio Santos <antonio.santos@runtime-revolution.com>
2024-09-16 17:10:37 +0100

    Remove user profile

--- db/migrate/20240913215634_create_user_profiles.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserProfiles < ActiveRecord::Migration[7.2]
-  def change
-    create_table :user_profiles do |t|
-      t.references :user, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
