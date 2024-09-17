f157d320ab641cedb0dca165a0184a0fd7791173
Antonio Santos <antonio.santos@runtime-revolution.com>
2024-09-17 10:07:22 +0100

    Add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   has_one :buyer
   has_one :seller
+
+  validates :username,
+            presence: true
+
+  validates :email,
+            presence: true,
+            email: true
+
+  validates :enabled,
+    presence: true
 end
