c69713e521da5b067574f8eaa50e4472026ed1f1
Antonio Santos <antonio.santos@runtime-revolution.com>
2024-09-20 11:39:38 +0100

    Add job to resize an image

---
+++ app/jobs/resize_image_job.rb
@@ -0,0 +1,12 @@
+class ResizeImageJob < ApplicationJob
+  queue_as :default
+
+  def perform(class_name, record_id, attachment_name, width, height)
+    klass = Object.const_get(class_name)
+    record = klass.find(record_id)
+    attachment = record.__send__(attachment_name)
+    return unless attachment.attached?
+
+    attachment.variant(resize_to_limit: [ 300, 300 ]).processed # process and cache the variant
+  end
+end
