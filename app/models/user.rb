db96b1e1a1324b87d63a488a0ef3187b45551c3e
Antonio Santos <antonio.santos@runtime-revolution.com>
2024-09-20 11:40:14 +0100

    Resize avatar after uploading it

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,11 @@ class User < ApplicationRecord
 
   validates :enabled,
     presence: true
+
+  after_save_commit :resize_avatar,
+                    if: -> { avatar.attached? }
+
+  def resize_avatar
+    ResizeImageJob.perform_later(self.class.name, self.id, :avatar, 250, 250)
+  end
 end
