957a078ac0b3e4e550d74dd803fe8ab4b27b8943
Antonio Santos <antonio.santos@runtime-revolution.com>
2024-09-24 17:34:04 +0100

    Users are enabled by default

--- db/schema.rb
+++ db/schema.rb
@@ -105,7 +105,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_23_112046) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.boolean "enabled"
+    t.boolean "enabled", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
