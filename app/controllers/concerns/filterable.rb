5080ac7beee95d3465139ddb5c9f56d9b9ef7f41
Antonio Santos <antonio.santos@runtime-revolution.com>
2024-09-24 17:18:21 +0100

    Remove debug code

--- app/controllers/concerns/filterable.rb
+++ app/controllers/concerns/filterable.rb
@@ -6,7 +6,6 @@ module Filterable
       filtering_params.reject! do |_, value|
         value.compact_blank!.empty? # Remove empty strings and query for empty arrays
       end
-      debugger
       results = self.where(nil) # same as self.all
       filtering_params.each do |key, value|
         results = results.public_send("filter_by_#{key}", value) if value.present?
