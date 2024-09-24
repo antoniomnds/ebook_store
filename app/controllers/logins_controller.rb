9320ff4cca36a1f286c9512932791f0b7ccdd86e
Antonio Santos <antonio.santos@runtime-revolution.com>
2024-09-24 18:51:11 +0100

    Fix a safe navigation issue in the authentication

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -6,7 +6,7 @@ class LoginsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       session[:current_user_id] = user.id
       redirect_to params[:back_url] || root_path,
                   notice: "You have successfully logged in.",
