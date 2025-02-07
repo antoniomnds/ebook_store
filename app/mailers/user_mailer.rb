c80a52a8ccb2af661f1c69fc67d87c9f24e60e29
Antonio Santos <antoniomndsantos@gmail.com>
2025-02-07 15:56:41 +0000

    Send emails to the logged-in user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,24 +1,23 @@
 class UserMailer < ApplicationMailer
   default from: "antoniomndsantos@gmail.com"
-  default to: "antonio.santos@runtime-revolution.com"
 
   def welcome
     @user = params[:user]
 
-    mail(subject: "Welcome to the Ebook Store!")
+    mail(subject: "Welcome to the Ebook Store!", to: @user.email)
   end
 
   def notify_purchase
     @user = params[:user]
     @ebook = params[:ebook]
 
-    mail(subject: "Thank you for buying '#{@ebook.title}'")
+    mail(subject: "Thank you for buying '#{@ebook.title}'", to: @user.email)
   end
 
   def notify_ebook_statistics
     @user = params[:user]
     @ebook = params[:ebook]
 
-    mail(subject: "About your new Ebook '#{@ebook.title}'")
+    mail(subject: "About your new Ebook '#{@ebook.title}'", to: @user.email)
   end
 end
