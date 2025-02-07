10a590fdc8969443ccbf86ae508cd837f314e693
Antonio Santos <antoniomndsantos@gmail.com>
2025-02-07 16:00:43 +0000

    Change queue adapter to run jobs inline in the test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,6 +40,9 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # Run jobs inline, meaning the job gets executed immediately.
+  config.active_job.queue_adapter = :test
+
   # Unlike controllers, the mailer instance doesn't have any context about the
   # incoming request so you'll need to provide the :host parameter yourself.
   config.action_mailer.default_url_options = { host: "www.example.com" }
