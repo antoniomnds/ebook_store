6821e7a9822d396a2c4450f26a1dd6d48bf6f384
Antonio Santos <antoniomndsantos@gmail.com>
2025-02-19 13:00:10 +0000

    Allow disabled users to be seen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.active
+    @users = User.all
   end
 
   # GET /users/1
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.active.find(params[:id])
+      @user = User.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
