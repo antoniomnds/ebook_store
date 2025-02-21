e32d4cc7d91d69403234a987fb49b56cf18133d8
Antonio Santos <antoniomndsantos@gmail.com>
2025-02-21 23:56:01 +0000

    Remove unnecessary conversion to json in a spec

--- spec/services/api/new_york_times/review_fetcher_spec.rb
+++ spec/services/api/new_york_times/review_fetcher_spec.rb
@@ -5,10 +5,10 @@ require 'support/request_helper'
 
 RSpec.describe ::Api::NewYorkTimes::ReviewFetcher do
   let(:ebook) { build(:ebook) }
-  let(:mocked_review) { { results: [ { summary: Faker::Lorem.sentence } ] }.to_json }
 
   describe ".call" do
     it "fetches a review from the API" do
+      mocked_review = { "results" => [ { "summary" => Faker::Lorem.sentence } ] }
       stub = stubbed_request(:get, { title: ebook.title }, {}, mocked_review)
 
       expect(::Api::NewYorkTimes::ReviewFetcher.call(title: ebook.title)).to eq(mocked_review["results"])
