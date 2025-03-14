6a333d17de76c69dda7b8052eb07ea45088e259c
Antonio Santos <antoniomndsantos@gmail.com>
2025-03-14 23:27:50 +0000

    Alias the user factory

--- spec/factories/ebooks.rb
+++ spec/factories/ebooks.rb
@@ -9,6 +9,6 @@ FactoryBot.define do
     publication_date { Faker::Date.between(from: 40.years.ago, to: Date.today) }
     pages { Faker::Number.number(digits: 3) }
     isbn { "978-#{Faker::Number.number(digits: 10)}" }
-    association :owner, factory: :user
+    owner
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, aliases: [ :owner ]  do
     username { Faker::Internet.username }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
