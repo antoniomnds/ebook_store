3f5f976a9ba27c552eb0ca1d8dc6d71af0c7fcfb
Antonio Santos <antoniomndsantos@gmail.com>
2025-04-13 10:41:56 +0100

    Load and launch simplecov before any application code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start 'rails'
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
@@ -9,12 +12,10 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # return unless Rails.env.test?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'simplecov'
 require 'support/factory_bot'
 require 'webmock/rspec'
 require 'capybara/rspec'
 
-SimpleCov.start 'rails'
 WebMock.disable_net_connect!(allow_localhost: true)
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
